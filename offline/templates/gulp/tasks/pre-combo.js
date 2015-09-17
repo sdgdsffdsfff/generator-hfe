253167b3b2164212c7ab8b258f200e1edf4d2926
maquan <maquan@meituan.com>
2015-09-17 12:24:44 +0800

    update

--- offline/templates/gulp/tasks/pre-combo.js
+++ offline/templates/gulp/tasks/pre-combo.js
@@ -2,13 +2,11 @@
 
 module.exports = function (gulp, Plugin, config) {
 	gulp.task('pre-combo', function () {
+		var repoinfo = require('../../repo-info.json');
+		// var pc = require('../util/pre-combo');
 
-		var pc = require('../util/pre-combo');
-
-		gulp.src('src/**/*.html').pipe(pc())
+		gulp.src('src/**/*.html')
+			.pipe(Plugin.precombo(repoinfo))
 			.pipe(gulp.dest('build'));
-
-
-
 	})
 };
